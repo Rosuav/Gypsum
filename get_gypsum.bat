8d01e3b76e64d7a06a48886eaea1f86328e1ec10
Chris Angelico <rosuav@gmail.com>
2016-11-27 01:28:51 +1100

    Run updates in three steps rather than two, to avoid a feature Win 7 lacks

--- get_gypsum.bat
+++ get_gypsum.bat
@@ -1,15 +1,16 @@
+@echo off
 cd %TMP%
 
 rem Download the Pike installer and Gypsum's archive
 rem http://superuser.com/a/760010
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://pike.lysator.liu.se/pub/pike/all/7.8.866/Pike-v7.8.866-win32.msi', 'pike.msi') }"
-start pike.msi
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/Rosuav/Gypsum/archive/master.zip', 'gypsum.zip') }"
+start /wait pike.msi
+mkdir c:\Gypsum
+mkdir c:\Gypsum\plugins
+cd c:\Gypsum
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://rosuav.github.io/Gypsum/plugins/update.pike', 'plugins\update.pike') }"
 
-rem Unzip Gypsum into a convenient directory
-rem http://stackoverflow.com/a/26843122/1236787
-powershell -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('gypsum.zip', 'c:\'); }"
-ren c:\gypsum-master Gypsum
+plugins\update.pike
 
 rem Create a shortcut. In theory, WindowStyle=7 should give a minimized window.
 rem TODO: Find the desktop directory even if it isn't obvious.
